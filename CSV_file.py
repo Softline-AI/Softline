5abb4f5b236d414ee897ae53d5bdfbf839cdb4c1
Softline <alehandro.og@bk.ru>
2025-08-19 09:33:25 +0300

    Update and rename tailwind.config.js to CSV_file.py

---
+++ CSV_file.py
@@ -0,0 +1,15 @@
+import bot as pd
+ 
+# loading the dataset
+
+df= pd.read_csv 
+ 
+print(df) #prints Dataset
+# funtions
+print(df.tail())
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.column)
+print(df.shape())
+ m // Placeholder content for tailwind.config.js
